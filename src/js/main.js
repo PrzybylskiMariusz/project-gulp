fceb21131e922f4153f17d8e719f1c589338d163
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-07-04 00:08:31 +0200

    feat: add style to header section

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+let time = new Date();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let today = time.toLocaleDateString("pl-PL", options);
+let paragraph = document.querySelector('.header__date');
+paragraph.innerHTML = today;
