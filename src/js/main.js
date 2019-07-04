441afb819c9cc5b0cb350e648f0b555ca297bd7c
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-07-04 08:41:21 +0200

    feat: hamburger menu and media queries for header section

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,15 @@ let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' 
 let today = time.toLocaleDateString("pl-PL", options);
 let paragraph = document.querySelector('.header__date');
 paragraph.innerHTML = today;
+
+const navigationList = document.querySelector('.navigation__list--js');
+const navigationHamburger = document.querySelector('.navigation__hamburger--js');
+
+navigationHamburger.addEventListener('click', (e) => {
+    navigationList.classList.toggle('navigation__list--visible');
+    if(navigationList.classList.contains('navigation__list--visible')){
+        navigationHamburger.innerHTML = 'X';
+    } else {
+        navigationHamburger.innerHTML = "&#9776;";
+    }
+});
